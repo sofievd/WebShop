a62ad355b414a12b2fe9017cd5a56cd134990429
Depinder Kaur <depinder.kaur@iths.se>
2024-03-14 16:46:23 +0100

    Created UserController and added basic functionality to show registration form, process the form (by checking for unique email), showing registration confirmation.

---
+++ src/main/java/se/iths/webshop/UserController.java
@@ -0,0 +1,60 @@
+package se.iths.webshop;
+
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+/**
+ * @author Depinder Kaur
+ * @version 0.1
+ * <h2>UserController</h2>
+ * @date 2024-03-14
+ */
+
+@Controller
+public class UserController {
+
+    @Autowired
+    UserService userService;
+
+    @GetMapping("/showForm")
+    public String showRegistrationForm (Model model) {
+
+        model.addAttribute("user", new User());
+        return "registration-form";
+    }
+
+    @PostMapping("/processForm")
+    public String processForm (@Valid @ModelAttribute("user") User user,
+                              BindingResult theBindingResult) {
+
+        if (userService.emailAlreadyExists(user.getEmail())) {
+            theBindingResult.rejectValue("email", "error.email", "Email already exists!");
+        }
+
+        if (theBindingResult.hasErrors()) {
+            return "registration-form";
+        } else {
+            userService.addUser(user.getEmail(), user.getPassword(), user.getFirstName(), user.getLastName());
+            return "customer-confirmation";
+        }
+    }
+
+    // add an InitBinder ... to convert trim input strings
+    // remove leading and trailing whitespace
+    // resolve issue for our validation
+    @InitBinder
+    public void initBinder(WebDataBinder dataBinder) {
+
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+        dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+    }
+}
