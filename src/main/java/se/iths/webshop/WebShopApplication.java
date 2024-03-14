c2bf1a25d6bfce23a258f24df3ba26ede8821eed
Sofie Van Dingenen <sofie.van.dingenen@iths.se>
2024-03-14 10:20:48 +0100

    Intital Commit

---
+++ src/main/java/se/iths/webshop/WebShopApplication.java
@@ -0,0 +1,13 @@
+package se.iths.webshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/iths/webshop/WebShopApplicationTests.java
@@ -0,0 +1,13 @@
+package se.iths.webshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
