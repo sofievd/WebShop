c674a121a5a3670627939c341412f51bc8fc7fca
Depinder Kaur <depinder.kaur@iths.se>
2024-03-15 10:31:11 +0100

    Created packages- controller, entity, repository and util.

--- src/main/java/se/iths/webshop/service/UserService.java
+++ src/main/java/se/iths/webshop/service/UserService.java
@@ -1,7 +1,10 @@
-package se.iths.webshop;
+package se.iths.webshop.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import se.iths.webshop.entity.User;
+import se.iths.webshop.repository.UserRepository;
+import se.iths.webshop.util.PasswordEncryptor;
 
 /**
  * @author Depinder Kaur
@@ -22,11 +25,13 @@ public class UserService {
 
     public User addUser(String email, String password, String firstName, String lastName) {
         User user = new User(email, password, firstName, lastName);
+        String hashPassword = PasswordEncryptor.hashPassword(password);
+        user.setPassword(hashPassword);
         userRepo.save(user);
         return user;
     }
 
-    public boolean emailAlreadyExists (String email) {
+    public boolean emailAlreadyExists(String email) {
         return userRepo.existsById(email);
     }
 
