0f71d2609e37a63e20460a179ef4f9101fca321b
Depinder Kaur <depinder.kaur@iths.se>
2024-03-16 22:27:22 +0100

    Created a new class WebProduct to handle the creation of new product by admin.

---
+++ src/main/java/se/iths/webshop/controller/model/WebProduct.java
@@ -0,0 +1,82 @@
+package se.iths.webshop.controller.model;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
+/**
+ * @author Depinder Kaur
+ * @version 0.1
+ * <h2>WebProduct</h2>
+ * @date 2024-03-16
+ */
+public class WebProduct {
+
+    @NotNull(message="is required")
+    private String name;
+
+    @NotNull(message="is required")
+    @Min(value=0, message="must be greater than or equal to 0")
+    @Max(value=2000, message="must be less than or equal to 2000")
+    private double price;
+
+    @NotNull(message="is required")
+    private String category;
+
+    @NotNull(message="is required")
+    private String description;
+
+    @NotNull(message="is required")
+    private String brand;
+
+    public WebProduct() {
+    }
+
+    public WebProduct(String name, double price, String category, String description, String brand) {
+        this.name = name;
+        this.price = price;
+        this.category = category;
+        this.description = description;
+        this.brand = brand;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+}
