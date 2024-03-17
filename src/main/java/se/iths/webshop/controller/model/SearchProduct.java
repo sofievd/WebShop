19acee5f79716d2cf2aa5e2477a8fcfa9231fffe
Depinder Kaur <depinder.kaur@iths.se>
2024-03-17 17:04:01 +0100

    Added a new class "SearchProduct" in the controller to search a product by name, brand and category.

---
+++ src/main/java/se/iths/webshop/controller/model/SearchProduct.java
@@ -0,0 +1,54 @@
+package se.iths.webshop.controller.model;
+
+import jakarta.validation.constraints.NotNull;
+
+/**
+ * @author Depinder Kaur
+ * @version 0.1
+ * <h2>SearchProduct</h2>
+ * @date 2024-03-17
+ */
+public class SearchProduct {
+
+    @NotNull(message="is required")
+    private String name;
+
+    @NotNull(message="is required")
+    private String category;
+
+    @NotNull(message="is required")
+    private String brand;
+
+    public SearchProduct() {
+    }
+
+    public SearchProduct(String name, String category, String brand) {
+        this.name = name;
+        this.category = category;
+        this.brand = brand;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+}
