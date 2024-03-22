87aaaf90740d646cc6afb807243d82b4f7f0683e
Sofie Van Dingenen <sofie.van.dingenen@iths.se>
2024-03-22 18:25:39 +0100

    small change to webshop page to search for a product.

--- src/main/java/se/iths/webshop/controller/ProductController.java
+++ src/main/java/se/iths/webshop/controller/ProductController.java
@@ -1,5 +1,6 @@
 package se.iths.webshop.controller;
 
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.parameters.P;
