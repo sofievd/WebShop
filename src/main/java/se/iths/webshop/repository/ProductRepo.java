9d6837885167d288bbc6957d7eada5f1409e7967
Depinder Kaur <depinder.kaur@iths.se>
2024-03-22 16:27:59 +0100

    In the ProductRepo, modified findByName method which will return a list of products rather than a single product.

--- src/main/java/se/iths/webshop/repository/ProductRepo.java
+++ src/main/java/se/iths/webshop/repository/ProductRepo.java
@@ -2,6 +2,7 @@ package se.iths.webshop.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import se.iths.webshop.dto.DesiredProductDto;
 import se.iths.webshop.entity.Category;
 import se.iths.webshop.entity.Product;
 
@@ -10,7 +11,7 @@ import java.util.Optional;
 
 @Repository
 public interface ProductRepo extends JpaRepository<Product, Integer> {
-    Optional<Product> findByName(String name);
+    List<Product> findByName(String name);
 
     List<Product> findAllByCategory(Category category);
 
