301c5b60fced062ba48b8f191f86e37011b4f3f6
Depinder Kaur <depinder.kaur@iths.se>
2024-03-28 17:21:04 +0100

    Created a new service class: CartItemService which contains method to get list of items in a specific cart/order.

---
+++ src/main/java/se/iths/webshop/service/CartItemService.java
@@ -0,0 +1,40 @@
+package se.iths.webshop.service;
+
+import org.springframework.stereotype.Service;
+import se.iths.webshop.dto.CartItem;
+import se.iths.webshop.entity.OrderLine;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Depinder Kaur
+ * @version 0.1
+ * <h2>CartItemService</h2>
+ * @date 2024-03-28
+ */
+@Service
+public class CartItemService {
+
+    public List<CartItem> getCartItemsList(List<OrderLine> orderLineList) {
+        List<CartItem> cartItemsList = new ArrayList<>();
+
+        for (OrderLine orderLine: orderLineList) {
+            CartItem cartItem = new CartItem();
+            cartItem.setProductName(orderLine.getProduct().getName());
+            cartItem.setPrice(orderLine.getProduct().getPrice());
+            cartItem.setQuantity(orderLine.getAmount());
+            cartItem.setTotalPrice(orderLine.getProduct().getPrice() * orderLine.getAmount());
+            cartItemsList.add(cartItem);
+        }
+        return cartItemsList;
+    }
+
+    public int getTotalNumberOfItemsInCart(List<CartItem> cartItemsList) {
+        int totalItemsInCart = 0;
+        for (CartItem item: cartItemsList) {
+            totalItemsInCart = totalItemsInCart + item.getQuantity();
+        }
+        return totalItemsInCart;
+    }
+}
