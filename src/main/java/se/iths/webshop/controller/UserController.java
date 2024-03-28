9d64d2f44415ab518dad84a73600ad2b7a985b65
Depinder Kaur <depinder.kaur@iths.se>
2024-03-28 20:01:18 +0100

    Created UserController to show all previous orders to the user.

---
+++ src/main/java/se/iths/webshop/controller/UserController.java
@@ -0,0 +1,39 @@
+package se.iths.webshop.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import se.iths.webshop.entity.Order;
+import se.iths.webshop.service.OrderService;
+
+import java.util.List;
+
+/**
+ * @author Depinder Kaur
+ * @version 0.1
+ * <h2>UserController</h2>
+ * @date 2024-03-28
+ */
+@Controller
+@RequestMapping("/customer")
+public class UserController {
+
+    @Autowired
+    private OrderService orderService;
+
+    @GetMapping("/allOrders")
+    public String allOrders(Model model) {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String loggedInUserEmail = authentication.getName();
+
+        List<Order> allOrdersOfUser = orderService.getAllOrdersOfUser(loggedInUserEmail);
+        model.addAttribute("ordersList", allOrdersOfUser);
+        return "customer/orders-list";
+    }
+
+}
--- src/main/java/se/iths/webshop/repository/OrderRepo.java
+++ src/main/java/se/iths/webshop/repository/OrderRepo.java
@@ -4,6 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import se.iths.webshop.entity.Order;
 
+import java.util.List;
+
 @Repository
 public interface OrderRepo extends JpaRepository<Order, Integer> {
+
+    List<Order> findByUserEmail(String email);
 }
