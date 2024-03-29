5c82dcad2d2a67a00a44850c323a63ed8c656c23
Depinder Kaur <depinder.kaur@iths.se>
2024-03-29 10:41:55 +0100

    Created a native Query to display a customer's orders from latest to oldest.

--- src/main/java/se/iths/webshop/repository/OrderRepo.java
+++ src/main/java/se/iths/webshop/repository/OrderRepo.java
@@ -1,6 +1,7 @@
 package se.iths.webshop.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import se.iths.webshop.entity.Order;
 
@@ -9,5 +10,7 @@ import java.util.List;
 @Repository
 public interface OrderRepo extends JpaRepository<Order, Integer> {
 
+    @Query(nativeQuery = true,
+            value = "select * from webshop.orders where customer_email=:email order by orders.id desc")
     List<Order> findByUserEmail(String email);
 }
