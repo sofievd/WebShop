2464e7aa92ff26fba558810cd4c517708ff9ab1d
Sofie Van Dingenen <sofie.van.dingenen@iths.se>
2024-04-05 15:57:35 +0200

    Update search for products .

--- src/main/java/se/iths/webshop/service/ProductService.java
+++ src/main/java/se/iths/webshop/service/ProductService.java
@@ -1,33 +1,47 @@
 package se.iths.webshop.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Service;
 import se.iths.webshop.entity.Category;
 import se.iths.webshop.entity.Product;
 import se.iths.webshop.repository.CategoryRepo;
 import se.iths.webshop.repository.ProductRepo;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Service
 public class ProductService {
-   @Autowired
+    @Autowired
     private ProductRepo productRepo;
-   @Autowired
+    @Autowired
     private CategoryRepo categoryRepo;
 
-    public List<Product> getProducts(){
+    public List<Product> getProducts() {
         return productRepo.findAll();
     }
-    public List<Product> getProductByCategory(String categoryString){
-         Category category = categoryRepo.findByName(categoryString);
-         List<Product> productsList = productRepo.findAllByCategory(category);
-         return productsList;
+
+    public List<Product> getProductByCategory(String categoryString) {
+        Category category = categoryRepo.findByName(categoryString);
+        List<Product> productsList = productRepo.findAllByCategory(category);
+        return productsList;
     }
 
-    public List<Product> searchProducts(String name){
-        return productRepo.findByName(name);
+    public List<Product> searchProducts(String searchWord) {
+        List<Product> allProducts = productRepo.findAll();
+        List<Product> productsfound = new ArrayList<>();
+        for(int i = 0; i < allProducts.size(); i++ ){
+            String name = allProducts.get(i).getName().toLowerCase();
+            String searchWordBegin = searchWord.toLowerCase().substring(0,3);
+            if(name.contains(searchWordBegin)){
+                productsfound.add(allProducts.get(i));
+            }
+        }
+        return productsfound;
     }
 
     public void saveProduct(Product product) {
